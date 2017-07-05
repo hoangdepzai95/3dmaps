348fde31166c2d6f880434d8b75fdbc9242013cb
Nguyễn Quang Hoàng <datinhcombat@gmail.com>
2017-07-06 00:14:08 +0700

    add eslint

--- src/sagas/login.js
+++ src/sagas/login.js
@@ -2,7 +2,7 @@ import { takeEvery, takeLatest } from 'redux-saga';
 import { take, put, call, fork, select, cancelled } from 'redux-saga/effects';
 
 function* watchResetPassword (){
-	yield takeLatest(RESET_PASSWORD, resetPassword )
+	yield takeLatest(RESET_PASSWORD, resetPassword );
 }
 
 export function* login(){
