713b1ae7d7cdc81a7ab7315dce3ceda6a9f93a36
Nguyễn Quang Hoàng <datinhcombat@gmail.com>
2017-07-11 07:21:51 +0700

    test map view

--- src/scenes/map/index.js
+++ src/scenes/map/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { MapView } from 'expo';
 import { Text, View } from 'react-native';
 
+import styles from './style';
+
 class Map extends Component {
   constructor(props) {
     super(props);
@@ -12,19 +14,62 @@ class Map extends Component {
         longitudeDelta: 0.04,
         latitudeDelta: 0.09,
       },
+      posts: [
+        {
+          coordinate: {
+            longitude: -122,
+            latitude: 37.01,
+          },
+          title: 'post1',
+        },
+        {
+          coordinate: {
+            longitude: -122,
+            latitude: 37.04,
+          },
+          title: 'post2',
+        },
+        {
+          coordinate: {
+            longitude: -122,
+            latitude: 37.06,
+          },
+          title: 'post3',
+        },
+      ],
     };
   }
   onRegionChangeComplete = (region) => {
     this.setState({ region });
   }
+  onPressCallOut = () => {
+    console.log('press');
+  }
+  renderPost = (posts) => {
+    return (
+      posts.map((post) => {
+        return (
+          <MapView.Marker coordinate={post.coordinate} key={post.title} pinColor="#42b983">
+            <MapView.Callout onPress={this.onPressCallOut}>
+              <View>
+                <Text>{post.title}</Text>
+                <Text>{post.title}</Text>
+              </View>
+            </MapView.Callout>
+          </MapView.Marker>
+        );
+      })
+    );
+  }
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <View style={styles.container}>
         <MapView
           style={{ flex: 1 }}
-          region={this.state.region}
-          onRegionChangeComplete={this.onRegionChangeComplete}
-        />
+          initialRegion={this.state.region}
+        >
+          {this.renderPost(this.state.posts)}
+        </MapView>
       </View>
     );
   }
